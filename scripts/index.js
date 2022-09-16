5d508bd72a57f3bd85435e5a18061f83cdb05a6b
vladimirgromozdin <70908406+vladimirgromozdin@users.noreply.github.com>
2022-09-16 20:10:01 +0300

    Added profileEdit popup inputs placeholders based on the content of the page

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,50 @@
+// Заполняем поля ввода формы «Редактировать профиль» данными со страницы
+
+// Ищем данные на странице и наполняем ими переменные
+let profileUsername = document.querySelector(".profile__username").textContent;
+console.log(profileUsername);
+
+let profileStatus = document.querySelector(".profile__status").textContent;
+console.log(profileStatus);
+
+// Ищем инпуты в форме и наполняем их значения контентом со страницы
+let profileEditFormUsernameInput = document.querySelector('#username');
+console.log(profileEditFormUsernameInput);
+
+let profileEditFormStatusInput = document.querySelector('#status');
+console.log(profileEditFormUsernameInput);
+
+function profileEditFormInputsValues () {
+  profileEditFormUsernameInput.value = profileUsername;
+  profileEditFormStatusInput.value = profileStatus;
+}
+
+profileEditFormInputsValues();
+
 // Оживляем попап «Редактировать профиль»
 
-// Обозначаем переменные, в которых будут нужные нам элементы попапа «Редактировать профиль»
+// Объявляем переменные
 let profileEditPopup = document.querySelector('.profileedit-popup');
 
 let profileEditButton = document.querySelector('.profile__editname-button');
 
-let profileEditPopupCloseIcon = document.querySelector('.profileedit-popup__close-button');
+let profileEditPopupCloseIcon = document.querySelector(
+  '.profileedit-popup__close-button'
+);
 
-// Функция добавления класса, когда попап нужно открыть
+// Добавляем класс при открытии
 function showProfileEditPopup() {
-  profileEditPopup.classList.add('profileedit-popup_opened');
+  profileEditPopup.classList.add("profileedit-popup_opened");
 }
 
-// Открываем попап при клике на кнопку редактирования профиля
-profileEditButton.addEventListener('click', showProfileEditPopup);
+// Открываем попап при клике на иконку
+profileEditButton.addEventListener("click", showProfileEditPopup);
 
-// Функция удаления класса, когда попап нужно закрыть
+// Удаляем класс при закрытии
 function closeProfileEditPopup() {
-  profileEditPopup.classList.remove('profileedit-popup_opened');
+  profileEditPopup.classList.remove("profileedit-popup_opened");
 }
 
 // Закрываем попап при клике на крестик
-profileEditPopupCloseIcon.addEventListener('click', closeProfileEditPopup);
+profileEditPopupCloseIcon.addEventListener("click", closeProfileEditPopup);
+
