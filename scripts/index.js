09dc2c1563caeba55f7deda92d8dfed71f80e578
vladimirgromozdin <70908406+vladimirgromozdin@users.noreply.github.com>
2022-11-01 19:32:43 +0400

    cleaned up code a bit

--- scripts/index.js
+++ scripts/index.js
@@ -9,22 +9,23 @@ const profileEditPopupCloseIcon = document.querySelector(
   ".profileedit-popup__close-button"
 );
 const profileEditFormUsernameInput = document.querySelector("#username");
-profileEditFormUsernameInput.value = profileUsername.textContent;
 const profileEditFormStatusInput = document.querySelector("#status");
-profileEditFormStatusInput.value = profileStatus.textContent;
+const profileFormElement = document.querySelector(".form-profile-edit");
+
+profileEditFormUsernameInput.value = profileUsername.textContent; // Set Username Placeholder
+profileEditFormStatusInput.value = profileStatus.textContent; // Set Status Placeholder
 
 profileEditButton.addEventListener("click", showProfileEditPopup);
 profileEditPopupCloseIcon.addEventListener("click", closeProfileEditPopup);
-const profileFormElement = document.querySelector(".form-profile-edit");
 profileFormElement.addEventListener("submit", formProfileEditSubmitHandler);
 
-// Card Add
+
+// Adding New Cards
 const addCardPopup = document.querySelector(".addnewcard-popup");
 const addCardButton = document.querySelector(".profile__add-new-button");
 const addCardPopupCloseIcon = document.querySelector(
   ".addnewcard-popup__close-button"
 );
-
 const cardAddNameInput = document.querySelector("#place-name");
 const cardAddImageURLInput = document.querySelector("#place-image-link");
 const cardAddForm = document.querySelector(".form-addnewcard");
@@ -35,28 +36,23 @@ addCardPopupCloseIcon.addEventListener("click", closeAddCardPopup);
 cardAddForm.addEventListener("submit", cardAddSubmitHandler);
 
 /** Functions */
-
-// Show Profile Edit Popup
 function showProfileEditPopup() {
-  profileEditPopup.classList.add("profileedit-popup_opened");
+  profileEditPopup.classList.add("profileedit-popup_opened"); // Show Profile Edit Popup
 }
 
 function closeProfileEditPopup() {
-  // Close Profile Edit Popup
-  profileEditPopup.classList.remove("profileedit-popup_opened");
+  profileEditPopup.classList.remove("profileedit-popup_opened");  // Close Profile Edit Popup
 }
 
-// Show Add Card Popup
 function showAddCardPopup() {
-  addCardPopup.classList.add("addnewcard-popup_opened");
-  const addCardNamePlaceholder = document.querySelector(
-    ".form-addnewcard__input"
-  );
+  addCardPopup.classList.add("addnewcard-popup_opened"); // Show Add Card Popup
+  // const addCardNamePlaceholder = document.querySelector(
+  //   ".form-addnewcard__input"
+  // );
 }
 
-// Close Add Card Popup
 function closeAddCardPopup() {
-  addCardPopup.classList.remove("addnewcard-popup_opened");
+  addCardPopup.classList.remove("addnewcard-popup_opened"); // Close Add Card Popup
 }
 
 // Handle Profile Edit Submissions
@@ -73,7 +69,6 @@ function formProfileEditSubmitHandler(evt) {
 }
 
 // Handle Card Add Submissions
-
 function cardAddSubmitHandler(evt) {
   evt.preventDefault();
 
@@ -84,24 +79,19 @@ function cardAddSubmitHandler(evt) {
   let cardImage = cardElement.querySelector(".element__image");
   cardImage.src = cardAddImageURL;
   cardImage.alt = cardAddName;
-  // cardElement.querySelector(".element__image").src = cardAddImageURL;
-  // cardElement.querySelector(".element__image").alt = cardAddName;
   cardElement.querySelector(".element__caption").textContent = cardAddName;
   cardList.prepend(cardElement);
 
   cardAddNameInput.value = "";
   cardAddImageURLInput.value = "";
-  // отслеживаем клики по кнопке лайков
+
   const likeButton = document.querySelector(".element__like-button");
-  likeButton.addEventListener("click", toggleLikeButton);
+  likeButton.addEventListener("click", toggleLikeButton); // Tracking Like Button Clicks
 
-
-  // отслеживаем клики по кнопке удаления
-  const removeButton = document.querySelector('.element__trash-bin-button');
-  removeButton.addEventListener('click', removeCard);
+  const removeButton = document.querySelector(".element__trash-bin-button");
+  removeButton.addEventListener("click", removeCard); // Tracking Remove Button Clicks
 
   closeAddCardPopup();
-
 }
 
 // Create Initial Cards
@@ -128,15 +118,13 @@ likeButtons.forEach(function (item) {
   item.addEventListener("click", toggleLikeButton);
 });
 
-
 //Removing Cards
-
 function removeCard(evt) {
-  evt.target.closest('.element').remove();
-};
+  evt.target.closest(".element").remove();
+}
 
-const removeButtons = document.querySelectorAll('.element__trash-bin-button ');
+const removeButtons = document.querySelectorAll(".element__trash-bin-button ");
 
 removeButtons.forEach(function (item) {
-  item.addEventListener('click', removeCard);
+  item.addEventListener("click", removeCard);
 });
