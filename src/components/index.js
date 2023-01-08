4b3ae729414ecf92c4c5096cd749907d08b0d2e5
vladimirgromozdin <70908406+vladimirgromozdin@users.noreply.github.com>
2023-01-08 18:51:08 +0400

    feat: add submit button enablement based on the validity of the form and move error messages to the proper part of the form

--- src/components/index.js
+++ src/components/index.js
@@ -98,8 +98,15 @@ const hideInputError = (formElement, inputElement) => {
 
 // Функция, которая проверяет валидность поля
 const isValid = (formElement, inputElement) => {
+  // if pattern is mismatched, show custom error
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity('');
+  }
+
   if (!inputElement.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
+    // if input can't be validated, show default error message
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
     // Если проходит, скроем
@@ -107,11 +114,32 @@ const isValid = (formElement, inputElement) => {
   }
 };
 
+// Checking of there are invalid inputs
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+};
+
+// Toggles submit button off if there is an invalid input
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add('popup__submit-button_inactive');
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove('popup__submit-button_inactive');
+  }
+};
+
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+  toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement)
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
@@ -130,4 +158,6 @@ const enableValidation = () => {
 };
 
 // Вызовем функцию
+// TODO: Pass enableValidation settings to the function as an object
+// TODO: Fix the issue with profile form button not being active by default even when the name and description are there
 enableValidation();
